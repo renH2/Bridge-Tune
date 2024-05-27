d1173a021712a4e2a67dae824beb0f833c8af221
anonymous-APT-ai <renh2@zju.edu.cn>
2024-05-27 16:42:30 +0800

    code

--- gcc/models/emb/__init__.py
+++ gcc/models/emb/__init__.py
@@ -1,11 +1,7 @@
 from .from_numpy import FromNumpy, FromNumpyAlign, FromNumpyGraph, Zero
-from .graphwave import GraphWave
-from .prone import ProNE
 
 __all__ = [
     "Zero",
-    "GraphWave",
-    "ProNE",
     "FromNumpy",
     "FromNumpyGraph",
     "FromNumpyAlign",
